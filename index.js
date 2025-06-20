4159317ec960af21004d14fad8f2c85ea8162bd7
Gani0230 <ganeshjadhav7478@gmail.com>
2025-06-20 11:18:01 +0530

    update in package.json file

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import express from 'express'
 import router from './routes/router.js'
 const app = express()
 import cors from 'cors'
+const PORT = process.env.PORT || 8080
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/v1", router)
 
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log("app is running on port 3000")
 })
